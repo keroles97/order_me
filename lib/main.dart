35e94ae16ce3580201afd16e36ab3dcf294bf7a0
keroles-ashraf <keroles97105@gmail.com>
2021-08-10 19:30:04 +0200

    jjjj

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'order',
+      title: 'اوردر مي سير',
       theme: ThemeData(
         primarySwatch: Colors.yellow,
       ),
